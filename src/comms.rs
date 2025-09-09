f64b8f36186dc09505b83c747cf8a665578ebed9
Jordan Rancie <8184815+jordanrancie@users.noreply.github.com>
2025-09-09 16:18:58 +0800

    REmoving old code

--- src/comms.rs
+++
@@ -1,8 +0,0 @@
-//! # API Module
-//!
-//! This module contains the communication layer for Massive Graph.
-//! It is responsible for sending and receiving messages between the
-//! different components of the system.
-
-
-pub mod webrtc;
--- src/system.rs
+++
@@ -1,8 +0,0 @@
-//! System utilities and monitoring
-//! 
-//! This module contains monitoring, profiling, health checks, and other
-//! system-level utilities.
-
-pub mod metrics;
-pub mod profiling;
-pub mod utils;
